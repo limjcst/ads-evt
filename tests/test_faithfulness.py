188508a81d3d4e3bf03eb0a99ac5842b1e00f5c7
limjcst <limjcst@163.com>
2021-12-19 09:58:52 +0800

    test: Test the faithfulness to the original one
    Signed-off-by: limjcst <limjcst@163.com>

---
+++ tests/test_faithfulness.py
@@ -0,0 +1,126 @@
+"""
+The refacted SPOT shall have the same output as the original ones
+"""
+import os
+
+import numpy as np
+import pandas as pd
+
+import pytest
+
+import ads_evt as spot
+
+from .spot_origin import spot as spot_origin
+
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+SPOT_ORIGIN_DIR = os.path.join(BASE_DIR, "spot_origin")
+
+
+def _read_dat(filename: str) -> np.ndarray:
+    with open(filename, encoding="UTF-8") as obj:
+        return np.array(list(map(float, obj.read().split(","))))
+
+
+class Dataset:
+    """
+    Uniform interface for dataset
+    """
+
+    def __init__(
+        self, init_data: np.ndarray, data: np.ndarray, proba: float, depth: int
+    ):
+        self.init_data = init_data
+        self.data = data
+        self.proba = proba
+        self.depth = depth
+
+    @staticmethod
+    def prepare_physics() -> "Dataset":
+        """
+        Prepare dataset from physics.dat
+        """
+        data = _read_dat(os.path.join(SPOT_ORIGIN_DIR, "physics.dat"))
+        return Dataset(init_data=2000, data=data, proba=1e-3, depth=450)
+
+    @staticmethod
+    def prepare_rain() -> "Dataset":
+        """
+        Prepare dataset from rain.dat
+        """
+        data = _read_dat(os.path.join(SPOT_ORIGIN_DIR, "rain.dat"))
+        return Dataset(init_data=1000, data=data, proba=1e-4, depth=200)
+
+    @staticmethod
+    def prepare_mawi() -> "Dataset":
+        """
+        Prepare dataset from MAWI
+        """
+        data_17 = pd.read_csv(os.path.join(SPOT_ORIGIN_DIR, "mawi_170812_50_50.csv"))
+        data_18 = pd.read_csv(os.path.join(SPOT_ORIGIN_DIR, "mawi_180812_50_50.csv"))
+        n_init = 1000
+        return Dataset(
+            init_data=data_17["rSYN"].values[-n_init:],
+            data=data_18["rSYN"].values,
+            proba=1e-4,
+            depth=200,
+        )
+
+    @staticmethod
+    def prepare_edf_stocks() -> "Dataset":
+        """
+        Prepare dataset from MAWI
+        """
+        edf_stocks = pd.read_csv(os.path.join(SPOT_ORIGIN_DIR, "edf_stocks.csv"))
+
+        # stream
+        u_data = edf_stocks["DATE"] == "2017-02-09"
+        data: np.ndarray = edf_stocks["LOW"][u_data].values
+
+        # initial batch
+        u_init_data = (
+            (edf_stocks["DATE"] == "2017-02-08")
+            | (edf_stocks["DATE"] == "2017-02-07")
+            | (edf_stocks["DATE"] == "2017-02-06")
+        )
+        init_data: np.ndarray = edf_stocks["LOW"][u_init_data].values
+
+        return Dataset(init_data=init_data, data=data, proba=1e-5, depth=10)
+
+
+def _compare(model: spot.SPOTBase, model_origin: spot.SPOTBase, dataset: Dataset):
+    results = []
+    for alg in [model, model_origin]:
+        alg.fit(init_data=dataset.init_data, data=dataset.data)
+        alg.initialize()
+        results.append(alg.run())
+
+    assert results[0] == results[0]
+
+
+@pytest.mark.parametrize(
+    "dataset",
+    [
+        Dataset.prepare_physics(),
+        Dataset.prepare_rain(),
+        Dataset.prepare_mawi(),
+        Dataset.prepare_edf_stocks(),
+    ],
+)
+def test_spot(dataset: Dataset):
+    """
+    End-to-end comparison
+    """
+    _compare(spot.SPOT(q=dataset.proba), spot_origin.SPOT(q=dataset.proba), dataset)
+    _compare(
+        spot.dSPOT(q=dataset.proba, depth=dataset.depth),
+        spot_origin.dSPOT(q=dataset.proba, depth=dataset.depth),
+        dataset,
+    )
+    _compare(spot.biSPOT(q=dataset.proba), spot_origin.biSPOT(q=dataset.proba), dataset)
+    # The original implementation of bidSPOT uses n_points=8 for _grimshaw by default
+    _compare(
+        spot.bidSPOT(q=dataset.proba, depth=dataset.depth, n_points=8),
+        spot_origin.bidSPOT(q=dataset.proba, depth=dataset.depth),
+        dataset,
+    )
